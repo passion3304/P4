1e8a52b310ee53941c0824904d7c7856cbd8a1ec
passion3304 <specialman3304@gmail.com>
2015-12-04 10:40:59 -0500

    Added database migrations

--- resources/views/welcome/index.blade.php
+++ resources/views/welcome/index.blade.php
@@ -7,7 +7,7 @@
 @section('content')
    <div class="container theme-showcase" role="main">
    <h1><strong>TaskMaster</strong></h1>
-   <p>This Laravel application provides users an interface to track personal tasks. Application features include:</p>
+   <p>This Laravel application provides a user interface to track personal tasks. Application features include:</p>
    <ul class="col-md-10">
       <li>User authentication so different users can have their own task lists.</li>
       <li>A page to display all incomplete tasks.</li>
